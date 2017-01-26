7c85f65e4d3c98193ce82c77bfb0d606098d1da6
TheEggHealer <TheEggKiller@gmail.com>
2017-01-26 17:04:42 +0100

    Fixed Resources

---
+++ Rotpot/Rotpot/src/utils/TileHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Svennebanan
+{
+    public class TileHandler
+    {
+
+        private Dictionary<int, Tile> tiles = new Dictionary<int, Tile>();
+
+        public void AddTile(Tile tile)
+        {
+            int id = (tile.texturePosition.X + (tile.texturePosition.Y * 8)) / 32;
+            tiles.Add(id, tile);
+        }
+
+        public Tile GetTile(int id)
+        {
+            return tiles[id];
+        }
+    }
+}

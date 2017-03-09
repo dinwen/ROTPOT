ec613c2a05eb49e205883945bd66fb0a1fdf9ed7
Daniel H <jomesa87@live.se>
2017-03-09 11:12:54 +0100

    Stuff

---
+++ Rotpot/Rotpot/src/gui/HUD.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Svennebanan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rotpot.src.gui
+{
+    public class HUD
+    {
+        public int x, y;
+        private Texture2D ui;
+        private Texture2D healthbar;
+        private Texture2D powerbar;
+
+        public HUD()
+        {
+            
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch batch)
+        {
+            batch.Draw()
+        }
+    }
+}

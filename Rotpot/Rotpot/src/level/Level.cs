5bee3ea41e9e3e72ce221249aed6030414447cb2
TheEggHealer <TheEggKiller@gmail.com>
2017-03-21 12:20:44 +0100

    Background & Other

--- Rotpot/Rotpot/src/level/Level.cs
+++ Rotpot/Rotpot/src/level/Level.cs
@@ -24,6 +24,8 @@ namespace Svennebanan
 
         public List<Tile> tiles = new List<Tile>();
 
+        private Vector2 bg1, bg2;
+
         public Level(ResourceManager resources)
         {
             this.resourceManager = resources;
@@ -36,27 +38,37 @@ namespace Svennebanan
 
             entityManager.AddEntity(this, new EntityPlayer(), 0);
             hud = new HUD(resources, this);
-            
+
+            Main.camera.SetLevelSize(levelLoader.size.X * 128, levelLoader.size.Y * 128);
         }
 
         public void Update(GameTime gameTime)
         {
             entityManager.Update(gameTime);
+
+            Main.camera.Update();
+            bg1 = new Vector2((int)((Main.camera.Position.X) / 1920) * 1920, Main.camera.Position.Y);
+            bg2 = new Vector2((int)((Main.camera.Position.X) / 1920 + 1) * 1920, Main.camera.Position.Y);
+        }
+
+        public EntityPlayer GetPlayer()
+        {
+            return (EntityPlayer) entityManager.GetEntity(0);
         }
 
         public void Draw(SpriteBatch batch)
         {
-
-
+            batch.Draw(resourceManager.images.GetImage("background"), bg1, Color.White);
+            batch.Draw(resourceManager.images.GetImage("background"), bg2, Color.White);
 
             entityManager.Draw(batch);
-            hud.Draw(batch);
 
             foreach (Tile t in tiles)
             {
-                
                 batch.Draw(resourceManager.images.GetImage("tile_sheet"), t.position, t.texturePosition, Color.White);
             }
+
+            hud.Draw(batch);
         }
 
     }
